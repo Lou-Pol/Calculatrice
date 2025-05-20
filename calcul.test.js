2bd14712d6906f1d1240244f5eff2b9ce8c70610
Lou Pol <lou.pol2006@gmail.com>
2025-05-20 12:16:36 +0200

    init

---
+++ calcul.test.js
@@ -0,0 +1,14 @@
+import { addition, soustraction,multiplication,division} from "./calculsafaire.js";
+
+test('addition', () => {
+    expect(addition(2,5)).toBe(7);
+})
+test('soustraction', () => {
+    expect(soustraction(2,5)).toBe(-3);
+})
+test('mult', () => {
+    expect(multiplication(2,5)).toBe(10);
+})
+test('addition', () => {
+    expect(division(12,4)).toBe(3);
+})
